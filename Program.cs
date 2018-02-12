95b844edee6b44180b6113cd88907f01c2b9c758
黃升煌 Mike <mike@miniasp.com>
2018-02-12 15:52:54 +0800

    FromLogContext

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Serilog;
+using Serilog.Context;
 using Serilog.Formatting.Compact;
 using Serilog.Formatting.Json;
 
@@ -11,6 +12,8 @@ namespace serilog_practice
         {
             using (var log = new LoggerConfiguration()
                 .MinimumLevel.Debug()
+                // 要使用LogContext.PushProperty, Enrich.FromLogcontext()為必要
+                .Enrich.FromLogContext()
                 .Enrich.WithProperty("Application", "Demo")
                 // Package: Serilog.Enrichers.Environment
                 .Enrich.WithMachineName()
@@ -34,11 +37,32 @@ namespace serilog_practice
                 log.Debug("Test");
                 log.Information("Hello, Serilog!");
                 log.Warning("Goodbye, Serilog.");
+
+                // 加入特定property
                 var specialLog = log.ForContext("Customer", "Mike");
                 specialLog.Warning("A very rich man! balance: {balance} millions", 100);
 
+                // 以某個Class為Property (property name: SourceProperty)
                 var contextLog = log.ForContext<SomeContext>();
                 contextLog.Warning("context log");
+
+                // 動態加入property
+                using (LogContext.PushProperty("OrderNumber", "ABC123"))
+                {
+                    using (LogContext.PushProperty("TransactionId", "T12345"))
+                    {
+                        log.Debug("this log has TransactionId");
+
+                        using (LogContext.PushProperty("OrderNumber", "ABC123"))
+                        {
+                            log.Debug("this log has OrderNumber");
+                        }
+                    }
+
+                    log.Debug("this log has no TransactionId");
+                }
+
+                log.Debug("this log has no OrderNumber");
             }
         }
     }
