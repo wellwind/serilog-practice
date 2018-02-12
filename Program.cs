b1c63b00fd064e1bcd6728b78762f4c332715b0f
黃升煌 Mike <mike@miniasp.com>
2018-02-12 12:55:58 +0800

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace serilog_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
